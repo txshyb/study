0244ad6e29938198eb3df74467d82db3ae655ad2
txshyb <15261279943hyb>
2019-10-30 23:22:00 +0800

    动态注册Redis连接类

--- spring-boot-test/src/main/java/com/txs/springboottest/动态实例化/Controller.java
+++ spring-boot-test/src/main/java/com/txs/springboottest/动态实例化/Controller.java
@@ -1,15 +1,22 @@
 package com.txs.springboottest.动态实例化;
 
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisShardInfo;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -38,5 +45,53 @@ public class Controller {
         System.out.printf(user2.toString());
         System.out.printf(user3.toString());
         return 2;
+    }
+
+    @RequestMapping("/setRedisConfig")
+    @ResponseBody
+    public void setRedisConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(2);
+        jedisPoolConfig.setMaxTotal(10);
+        jedisPoolConfig.setMaxWaitMillis(3000);
+        jedisPoolConfig.setMinIdle(1);
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setPoolConfig(jedisPoolConfig);
+        jedisConnectionFactory.setHostName("127.0.0.1");
+        jedisConnectionFactory.setPort(6379);
+//        jedisConnectionFactory.setPassword("future1234");
+        jedisConnectionFactory.setDatabase(8);
+        jedisConnectionFactory.setUsePool(true);
+        //TODO  与分布式有关 待查  不加上报错
+        JedisShardInfo jedisShardInfo = new JedisShardInfo("127.0.0.1",6379);
+
+        jedisConnectionFactory.setShardInfo(jedisShardInfo);
+
+        RedisTemplate template = new RedisTemplate();
+        template.setConnectionFactory(jedisConnectionFactory);
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+        template.setValueSerializer(serializer);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+
+        BeanFactory beanFactory = BeanFactoryHolder.getBeanFactory();
+        DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory)beanFactory;
+        defaultListableBeanFactory.registerSingleton("testRedis", template);
+    }
+
+    @RequestMapping("/getRedisConfig")
+    @ResponseBody
+    public Object getRedisConfig() {
+        //动态获取类
+        RedisTemplate redisTemplate = (RedisTemplate)ApplicationHolder.getApplication().getBean("testRedis");
+
+        Set keys = redisTemplate.keys("*");
+        return keys;
     }
 }
